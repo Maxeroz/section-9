a668f92597104527ff4ab31fa6b9e5aae7ba17cb
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-07-26 20:01:53 +0400

    Optional Chaining (?.)

--- script.js
+++ script.js
@@ -53,6 +53,44 @@ const restaurant = {
   },
 };
 
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+// if (restaurant.openingHours.fri) console.log(restaurant.openingHours.fri.open);
+// restaurant.openingHours.fri && console.log(restaurant.openingHours.fri.open);
+
+// Arrays
+// const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
+
+// console.log([]);
+/*
+///////////////////////////////////
+// The for-of Loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 // for (let i = 0; i < menu.length; i++) {
@@ -67,7 +105,6 @@ for (const [i, el] of menu.entries()) {
 }
 
 // console.log([...menu.entries()]);
-/*
 
 const rest1 = {
   name: 'Capri',
