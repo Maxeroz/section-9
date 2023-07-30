549f9e227dbd1ff5af0c6128d1953709c8a042a2
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-07-30 15:54:59 +0400

    Working With Strings - Part 2

--- script.js
+++ script.js
@@ -53,6 +53,92 @@ const restaurant = {
   },
 };
 
+/*
+/////////////////////////////
+// Working With Strings - Part 2
+
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnAS'; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Fixing capitalization function
+
+const correctF = function (name) {
+  const nameLower = name.toLowerCase();
+  const nameCorrect = nameLower[0].toUpperCase() + nameLower.slice(1);
+  console.log(nameCorrect);
+};
+
+correctF('MAKsiM');
+
+// Comparing emails
+
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Comparing Emails Function
+const compareEmailF = function (correctEmail, inputEmail) {
+  const correctE = correctEmail;
+  const inpE = inputEmail;
+  const normalizedE = inpE.toLowerCase().trim();
+  const comparasion = correctE === normalizedE;
+  return comparasion;
+};
+
+console.log(
+  compareEmailF('ozerskiy.maxim@gmail.com', 'ozerskiy.maxim@gmail.com')
+);
+
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+// console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on boarding');
+  } else {
+    console.log('Welcome aboard');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+/////////////////////////////
 // Working With Strings - Part 1
 
 const ariline = 'TAP Air Portugal';
@@ -94,7 +180,7 @@ checkMiddleSeat('3E');
 console.log(new String('jonas'));
 console.log(typeof new String('jonas'));
 console.log(typeof new String('jonas').slice(1));
-/*
+
 /////////////////////////////
 //Coding Challenge #3
 // Conditions
